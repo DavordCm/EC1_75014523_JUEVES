47429384788fd0cf6ad3cd40b831c26c5f29a3af
DavordCm <jhoosman.xd@gmail.com>
2024-10-10 21:49:45 -0500

    Creacion de los interfaces de Empresa y los impytes, activitys y el uso de binding

--- app/src/main/java/pe/edu/idat/ec1_sm75014523/Descuento_en_Almacen.kt
+++ app/src/main/java/pe/edu/idat/ec1_sm75014523/Descuento_en_Almacen.kt
@@ -6,21 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Descuento_en_Almacen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_descuento_en_almacen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        fun calcularPrecio(cantidad: Int, precioUnitario: Double): Double {
-            val total = cantidad * precioUnitario
-            val descuento = if (total > 200) total * 0.20 else 0.0
-            return total - descuento
-        }
-
     }
 }
